d48bdfb595fac0f6e826273628dfeadd6ca0195f
Marcial Ambriz <jmaramgo@gmail.com>
2024-07-25 23:39:02 -0700

    Update(Font): Body and display fonts and bootstrap text vars were configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,14 +6,14 @@ export default defineNuxtConfig({
   ssr: process.env.NODE_ENV !== 'development',
   runtimeConfig: {
     public: {
-      API_BASE_URL: process.env.API_URL
-    }
+      API_BASE_URL: process.env.API_URL,
+    },
   },
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'My amazing site',
+      title: 'Denthia',
       meta: [{ name: 'description', content: 'My amazing site' }],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -21,12 +21,20 @@ export default defineNuxtConfig({
         {
           rel: 'preconnect',
           href: 'https://fonts.gstatic.com',
-          crossorigin: 'anonymous'
-        }
-      ]
+          crossorigin: 'anonymous',
+        },
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap',
+          rel: 'stylesheet',
+        },
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+          rel: 'stylesheet',
+        },
+      ],
     },
     pageTransition: { name: 'page', mode: 'out-in' },
-    layoutTransition: { name: 'layout', mode: 'out-in' }
+    layoutTransition: { name: 'layout', mode: 'out-in' },
   },
   plugins: [{ src: 'plugins/bootstrap.js', mode: 'client' }],
 
@@ -36,10 +44,10 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/_variables.scss" as *;'
-        }
-      }
-    }
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
   },
 
   modules: ['@nuxtjs/i18n', '@pinia/nuxt', '@nuxt/eslint'],
@@ -49,17 +57,17 @@ export default defineNuxtConfig({
       {
         code: 'en',
         name: 'English',
-        file: 'en-US.json'
+        file: 'en-US.json',
       },
       {
         code: 'es',
         name: 'Español',
-        file: 'es-ES.json'
-      }
+        file: 'es-ES.json',
+      },
     ],
     lazy: true,
     langDir: 'locales',
     defaultLocale: 'en',
-    vueI18n: './lib/i18n.config.ts'
-  }
+    vueI18n: './lib/i18n.config.ts',
+  },
 });
